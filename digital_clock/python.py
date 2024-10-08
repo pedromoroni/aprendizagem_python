393d2031beb2cbe7cd8e0562554984efa308c8ba
moroni <pedromoroni2007@gmail.com>
2024-10-08 11:18:25 +0100

    Add files via upload

---
+++ digital_clock/python.py
@@ -0,0 +1,42 @@
+import sys
+import pyqt5
+
+class DigitalClock(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.time_label = QLabel(self)
+        self.timer = QTimer(self)
+        self.initUi()
+
+    def initUi(self):
+        self.setWindowTitle('Digital Clock')
+        self.setGeometry(600, 400, 300, 100)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.time_label)
+        self.setLayout(vbox)
+
+        self.time_label.setAlignment(Qt.AlignCenter)
+        self.time_label.setStyleSheet('font-size: 150px;'
+                                      'color: #22db18;')
+        self.setStyleSheet('background-color: black')
+
+        font_id = QFontDatabase.addApplicationFont('AtomicClockRadio.ttf')
+        font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
+        my_font = QFont(font_family, 150)
+        self.time_label.setFont(my_font)
+
+        self.timer.timeout.connect(self.update_time)
+        self.timer.start(1000)
+
+        self.update_time()
+
+    def update_time(self):
+        current_time = QTime.currentTime().toString('hh:mm:ss')
+        self.time_label.setText(current_time)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    clock = DigitalClock()
+    clock.show()
+    sys.exit(app.exec_())
